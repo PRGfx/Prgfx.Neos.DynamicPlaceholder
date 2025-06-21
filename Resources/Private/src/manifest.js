b935ec85b5df7467d8883dcd4f240f36a5122294
Matthias Klatte <matthias.klatte@gmail.com>
2025-06-21 15:45:02 +0200

    fix evaluation of dynamic placeholders
    Placeholders are no longer created through a plugin, so we have a
    different context to retrieve the node from.
    Also updates the way we evaluate ClientEval, resolving build warnings
    and using an implementation from the neos-ui to make sure, variables
    are in scope.

--- Resources/Private/src/manifest.js
+++ Resources/Private/src/manifest.js
@@ -2,6 +2,13 @@ import manifest from '@neos-project/neos-ui-extensibility';
 import { selectors } from '@neos-project/neos-ui-redux-store';
 
 /**
+ * from @neos-project/utils-helpers
+ * @param {string} value
+ * @return {string}
+ */
+const stripTags = (value) => value.replace(/<\/?[^>]+(>|$)/g, '');
+
+/**
  * Evaluates a ClientEval: expression with node and editorOptions in scope
  * @param {string} input The ClientEval expression
  * @param {object} node The node data
@@ -9,63 +16,50 @@ import { selectors } from '@neos-project/neos-ui-redux-store';
  * @returns {any}
  */
 const clientEval = (input, node, editorOptions) => {
-    return eval(input);
+    const evaluateFn = new Function('node,editorOptions', 'return ' + input);
+    return evaluateFn(node, editorOptions);
 }
 
-/**
- * Augments the NeosPlaceholder plugin
- * @param {Function} plugin The original plugin
- * @param {(contextPath: string) => object} findNode A helper to find the node data for evaluation
- * @param {object} editorOptions The inline editorOptions
- * @returns {Function} A decorated version of the plugin
- */
-const decoratePlaceholderPlugin = (plugin, findNode, editorOptions) => {
-    const originalGetPlaceholder = plugin.prototype.getPlaceholder;
-    plugin.prototype.getPlaceholder = function () {
-        const originalPlaceholder = originalGetPlaceholder.apply(this);
-        if (originalPlaceholder.startsWith('ClientEval:')) {
-            if (this.editor && this.editor.neos && this.editor.neos.contextPath) {
-                try {
-                    const node = findNode(this.editor.neos.contextPath);
-                    if (node) {
-                        return clientEval(originalPlaceholder.substring(11), node, editorOptions).toString();
-                    }
-                } catch (e) {
-                }
-            }
-        }
-        return originalPlaceholder;
-    };
-
-    // for some reason editor.neos is not yet available when initializing the plugin
-    const originalInit = plugin.prototype.init;
-    plugin.prototype.init = function() {
-        const init = originalInit.bind(this);
-        if (!this.editor || !this.editor.neos) {
-            setTimeout(() => init(), 100);
-        } else {
-            init();
-        }
-    };
-
-    return plugin;
-}
-
-manifest('Prgfx.Neos.DynamicPlaceholder', {}, (globalRegistry, { store }) => {
+manifest('Prgfx.Neos.DynamicPlaceholder', {}, (globalRegistry, {store}) => {
     const ckeRegistry = globalRegistry.get('ckEditor5');
     const ckeConfig = ckeRegistry.get('config');
-    const addExistingPlugin = ckeConfig.get('neosPlaceholder');
-    if (!addExistingPlugin) return;
+    const existingConfig = ckeConfig.get('baseConfiguration');
+    if (!existingConfig) return;
 
     const getNodeByContextPath = path => selectors.CR.Nodes.makeGetNodeByContextPathSelector(path)(store.getState());
 
-    const decorateAddedPlugin = (ckEditorConfiguration, options) => {
-        const r = addExistingPlugin(ckEditorConfiguration, options);
-        const index = r.plugins.findIndex(p => p.pluginName === 'NeosPlaceholder');
-        if (index >= 0) {
-            r.plugins[index] = decoratePlaceholderPlugin(r.plugins[index], getNodeByContextPath, options.editorOptions);
+    const decoratedConfig = (ckEditorConfiguration, options) => {
+        const {editorOptions, propertyDomNode} = options;
+        const baseConfig = existingConfig(ckEditorConfiguration, options);
+        const contextPath = propertyDomNode?.getAttribute('data-__neos-node-contextpath');
+        const originalPlaceholder = baseConfig.placeholder;
+
+        if (originalPlaceholder.startsWith('ClientEval:')) {
+            if (contextPath) {
+                const i18nRegistry = globalRegistry.get('i18n');
+                try {
+                    const node = getNodeByContextPath(contextPath);
+                    if (node) {
+                        let evaluatedPlaceholder = clientEval(originalPlaceholder.substring(11), node, editorOptions).toString();
+                        evaluatedPlaceholder = stripTags(i18nRegistry.translate(evaluatedPlaceholder));
+
+                        if (evaluatedPlaceholder) {
+                            return {
+                                ...baseConfig,
+                                placeholder: evaluatedPlaceholder,
+                            };
+                        }
+                    }
+                } catch (e) {
+                    console.warn('[Prgfx.Neos.DynamicPlaceholder] Could not evaluate ClientEval placeholder', originalPlaceholder, e);
+                }
+            } else {
+                console.warn('[Prgfx.Neos.DynamicPlaceholder] Could not find contextPath for ClientEval placeholder', originalPlaceholder);
+            }
         }
-        return r;
+
+        return baseConfig;
     };
-    ckeConfig.set('neosPlaceholder', decorateAddedPlugin);
+
+    ckeConfig.set('baseConfiguration', decoratedConfig);
 });
